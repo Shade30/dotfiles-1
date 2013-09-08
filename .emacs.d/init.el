67cdd9ce03a3443e943b6113a7ca5097e8f1b8c6
svfedorov <shade30@gmail.com>
2013-09-08 02:59:48 -0700

    Modified C-h and C-d (backspace)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -65,6 +65,7 @@
 (global-set-key "\M-i" 'back-window)
 (global-set-key "\C-z" 'zap-to-char)
 (global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\C-d" 'backward-delete-char)
 (global-set-key "\M-d" 'delete-word)
 (global-set-key "\M-h" 'backward-delete-word)
 (global-set-key "\M-u" 'zap-to-char)
